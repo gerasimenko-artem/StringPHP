ec876d1c8cbc3a5130be893061ef1c017ef5cf40
Artem <gerasimenko3452@gmail.com>
2024-07-19 00:11:17 +0300

    Init commit

---
+++ src/StringUtils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace StringUtils\src;
+class StringUtils
+{
+    public static function reverseString($string)
+    {
+        return strrev($string);
+    }
+
+    public static function changeCase($string, $case = 'upper')
+    {
+        return $case === 'upper' ? strtoupper($string) : strtolower($string);
+    }
+
+    public static function trimString($string)
+    {
+        return trim($string);
+    }
+
+    public static function isPalindrome($string)
+    {
+        $reversedString = strrev($string);
+        return $string === $reversedString;
+    }
+}
